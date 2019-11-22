10e0e9e1b29177bf386a470264c66ae3316639d1
Matthew Chick <c.chick@unsw.edu.au>
2019-11-22 12:18:23 +1100

    Initial version of tasks generated by JHipster-6.5.1

---
+++ src/main/java/com/matthew/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.matthew.repository;
+
+import com.matthew.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
